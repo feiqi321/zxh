baff7ad9a740904bec96392290c5f9f62d7b589d
齐飞 <qifei@ovfintech.com>
2019-02-11 09:10:48 +0800

    update

--- src/main/java/xyz/zaijushou/zhx/sys/web/FileManageController.java
+++ src/main/java/xyz/zaijushou/zhx/sys/web/FileManageController.java
@@ -4,6 +4,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.ServletOutputStream;
