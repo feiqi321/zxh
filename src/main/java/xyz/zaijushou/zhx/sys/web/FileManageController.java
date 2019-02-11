f0b0dd608ff336b5c43fa67b9bcc85f3aa326983
齐飞 <qifei@ovfintech.com>
2019-02-11 08:52:55 +0800

    update

--- src/main/java/xyz/zaijushou/zhx/sys/web/FileManageController.java
+++ src/main/java/xyz/zaijushou/zhx/sys/web/FileManageController.java
@@ -2,26 +2,15 @@ package xyz.zaijushou.zhx.sys.web;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
-import xyz.zaijushou.zhx.common.web.WebResponse;
-import xyz.zaijushou.zhx.sys.entity.DataCollectionEntity;
-import xyz.zaijushou.zhx.sys.entity.FileManageDTO;
-import xyz.zaijushou.zhx.sys.service.DataCollectionService;
-import xyz.zaijushou.zhx.sys.service.FileDownService;
 
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.InputStream;
 
 /**
  * Created by looyer on 2019/1/30.
@@ -29,8 +18,6 @@ import java.io.InputStream;
 @Api("文件上传下载管理")
 @RestController
 public class FileManageController {
-    @Autowired
-    private FileDownService fileDownService;
 
     @Value(value = "${rar_path}")
     private String filePath;
