9cc171344bfb754206eaee574e073eaee3aece42
qiuyiping <qiuyiping@aliyun.com>
2019-02-18 22:33:19 +0800

    导出

--- src/main/java/xyz/zaijushou/zhx/sys/web/FileManageController.java
+++ src/main/java/xyz/zaijushou/zhx/sys/web/FileManageController.java
@@ -46,7 +46,7 @@ public class FileManageController {
 
 
         //下载的文件携带这个名称
-        response.setHeader("Content-Disposition", URLEncoder.encode(fileName, "UTF-8"));
+        response.setHeader("Content-Disposition", "attachment;filename=" + URLEncoder.encode(fileName, "UTF-8"));
         //文件下载类型--二进制文件
         response.setContentType("application/octet-stream");
 
