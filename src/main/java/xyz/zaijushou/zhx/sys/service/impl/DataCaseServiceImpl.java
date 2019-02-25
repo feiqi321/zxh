3af01559eb086a60410158f39ffa560d139b1e32
齐飞 <qifei@ovfintech.com>
2019-02-25 21:58:44 +0800

    update

--- src/main/java/xyz/zaijushou/zhx/sys/service/impl/DataCaseServiceImpl.java
+++ src/main/java/xyz/zaijushou/zhx/sys/service/impl/DataCaseServiceImpl.java
@@ -277,6 +277,15 @@ public class DataCaseServiceImpl implements DataCaseService {
                         temp.setCollectStatusMsg(sysDictionaryEntity.getName());
                     }
                 }
+                if (org.apache.commons.lang3.StringUtils.isEmpty(temp.getCollectArea())){
+                    temp.setCollectArea("");
+                }else{
+                    List<SysDictionaryEntity> dictList = dictionaryMapper.getDataById(Integer.parseInt(temp.getCollectArea()));
+                    if (dictList.size() > 0) {
+                        SysDictionaryEntity sysDictionaryEntity = dictList.get(0);
+                        temp.setCollectArea(sysDictionaryEntity.getName());
+                    }
+                }
                 if (org.apache.commons.lang3.StringUtils.isEmpty(temp.getOdv())){
                     temp.setOdv("");
                 }else {
@@ -386,10 +395,18 @@ public class DataCaseServiceImpl implements DataCaseService {
 
     @Override
     public void sendOdv(DataCaseEntity bean){
+        SysUserEntity tempuser = new SysUserEntity();
+        tempuser.setId(Integer.parseInt(bean.getOdv()));
+        SysUserEntity user = sysUserService.findUserInfoWithoutStatusById(tempuser);
+        bean.setDistributeHistory("分配给"+user.getUserName());
         dataCaseMapper.sendOdv(bean);
     }
     @Override
     public void sendOdvByProperty(DataCaseEntity bean){
+        SysUserEntity tempuser = new SysUserEntity();
+        tempuser.setId(Integer.parseInt(bean.getOdv()));
+        SysUserEntity user = sysUserService.findUserInfoWithoutStatusById(tempuser);
+        bean.setDistributeHistory("分配给"+user.getUserName());
         dataCaseMapper.sendOdvByProperty(bean);
     }
     @Override
