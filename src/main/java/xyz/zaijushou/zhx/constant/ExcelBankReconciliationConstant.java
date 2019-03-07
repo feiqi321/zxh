a23de4421461ae1f8073773607260caae8d2932e
qiuyiping <qiuyiping@aliyun.com>
2019-03-08 07:43:48 +0800

    协催模块导出

--- src/main/java/xyz/zaijushou/zhx/constant/ExcelBankReconciliationConstant.java
+++ src/main/java/xyz/zaijushou/zhx/constant/ExcelBankReconciliationConstant.java
@@ -1,6 +1,9 @@
 package xyz.zaijushou.zhx.constant;
 
 import xyz.zaijushou.zhx.sys.entity.DataCaseEntity;
+import xyz.zaijushou.zhx.sys.entity.DataCaseRemarkEntity;
+import xyz.zaijushou.zhx.sys.entity.SysDictionaryEntity;
+import xyz.zaijushou.zhx.sys.entity.SysNewUserEntity;
 
 import java.math.BigDecimal;
 import java.util.Date;
@@ -68,7 +71,59 @@ public class ExcelBankReconciliationConstant {
     }
 
     public enum BankReconciliationExport implements ExcelEnum {
-        COL10(10, "ID", "id", Integer.class),
+        COL10(10, "还款ID", "id", Integer.class),
+        COL20(20, "批次号", "dataCase.batchNo", DataCaseEntity.class, String.class),
+        COL30(30, "卡号", "dataCase.cardNo", DataCaseEntity.class, String.class),
+        COL40(40, "证件号", "dataCase.identNo", DataCaseEntity.class, String.class),
+        COL50(50, "姓名", "dataCase.name", DataCaseEntity.class, String.class),
+        COL60(60, "个案序列号", "dataCase.seqNo", DataCaseEntity.class, String.class),
+        COL70(70, "委托方", "dataCase.client", DataCaseEntity.class, String.class),
+        COL80(80, "委案金额", "dataCase.money", DataCaseEntity.class, BigDecimal.class),
+        COL90(90, "当前委案余额", "dataCase.balance", DataCaseEntity.class, BigDecimal.class),
+        COL100(100, "账号", "dataCase.account", DataCaseEntity.class, String.class),
+        COL110(110, "档案号", "dataCase.archiveNo", DataCaseEntity.class, String.class),
+        COL120(120, "申请单号", "dataCase.applyOrderNo", DataCaseEntity.class, String.class),
+        COL130(130, "CP金额", "cpMoney", BigDecimal.class),
+        COL140(140, "CP日期", "cpDate", Date.class),
+        COL150(150, "案件催收员", ""),
+        COL160(160, "回收催收员", ""),
+        COL170(170, "回收部门", ""),
+        COL180(180, "提交人", "submitUser.userName", SysNewUserEntity.class, String.class),
+        COL190(190, "提交时间", "submitTime", Date.class),
+        COL200(200, "还款金额", "dataCase.repayMoney", DataCaseEntity.class, BigDecimal.class),
+        COL210(210, "还款日期", ""),
+        COL220(220, "还款余额", ""),
+        COL230(230, "M值金额", ""),
+        COL240(240, "M值系数", "dataCase.mVal", DataCaseEntity.class, BigDecimal.class),
+        COL250(250, "还款人", "repayUser", String.class),
+        COL260(260, "还款方式", "repayType.name", SysDictionaryEntity.class, String.class),
+        COL270(270, "确认人", ""),
+        COL280(280, "确认时间", ""),
+        COL290(290, "还款备注", ""),
+        COL300(300, "逾期账龄", "dataCase.accountAge", DataCaseEntity.class, String.class),
+        COL310(310, "逾期期数", "dataCase.overduePeriods", DataCaseEntity.class, Double.class),
+        COL320(320, "逾期日期", "dataCase.overdueDate", DataCaseEntity.class, Date.class),
+        COL330(330, "逾期天数", "dataCase.overdueDays", DataCaseEntity.class, Double.class),
+        COL340(340, "委案日期", "dataCase.caseDate", DataCaseEntity.class, String.class),
+        COL350(350, "预计退案日", "dataCase.expectTime", DataCaseEntity.class, Date.class),
+        COL360(360, "省份", "dataCase.province.name", DataCaseEntity.class, SysDictionaryEntity.class, String.class),
+        COL370(370, "城市", "dataCase.city.name", DataCaseEntity.class, SysDictionaryEntity.class, String.class),
+        COL380(380, "区县", "dataCase.county.name", DataCaseEntity.class, SysDictionaryEntity.class, String.class),
+        COL390(390, "手机", "dataCase.tel", DataCaseEntity.class, String.class),
+        COL400(400, "币种", "dataCase.currencyType", DataCaseEntity.class, String.class),
+        COL410(410, "分配历史", "dataCase.distributeHistory", DataCaseEntity.class, String.class),
+        COL420(420, "拖欠级别", "dataCase.defaultLevel", DataCaseEntity.class, String.class),
+        COL430(430, "案件最新欠款", "dataCase.latestOverdueMoney", DataCaseEntity.class, BigDecimal.class),
+        COL440(440, "外访状态", "dataCase.inteviewStatus", DataCaseEntity.class, String.class),
+        COL450(450, "公司佣金", "dataCase.commissionMoney", DataCaseEntity.class, BigDecimal.class),
+        COL460(460, "公司佣金比率", "dataCase.commissionRate", DataCaseEntity.class, Date.class),
+        COL470(470, "案件备注1", "dataCase.caseRemarks[0].name", DataCaseEntity.class, DataCaseRemarkEntity.class, String.class),
+        COL480(480, "案件备注2", "dataCase.caseRemarks[1].name", DataCaseEntity.class, DataCaseRemarkEntity.class, String.class),
+        COL490(490, "案件备注3", "dataCase.caseRemarks[2].name", DataCaseEntity.class, DataCaseRemarkEntity.class, String.class),
+        COL500(500, "案件备注4", "dataCase.caseRemarks[3].name", DataCaseEntity.class, DataCaseRemarkEntity.class, String.class),
+        COL510(510, "案件备注5", "dataCase.caseRemarks[4].name", DataCaseEntity.class, DataCaseRemarkEntity.class, String.class),
+        COL520(520, "案件备注6", "dataCase.caseRemarks[5].name", DataCaseEntity.class, DataCaseRemarkEntity.class, String.class),
+        COL530(530, "催收区域", "dataCase.collectionArea.name", DataCaseEntity.class, SysDictionaryEntity.class, String.class),
         ;
 
         private Integer sort;
