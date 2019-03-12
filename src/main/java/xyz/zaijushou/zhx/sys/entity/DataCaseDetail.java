970e4b965702eefbc0d8fc724bcf70aed9e4bdf0
齐飞 <qifei@ovfintech.com>
2019-03-12 14:35:03 +0800

    update

--- src/main/java/xyz/zaijushou/zhx/sys/entity/DataCaseDetail.java
+++ src/main/java/xyz/zaijushou/zhx/sys/entity/DataCaseDetail.java
@@ -73,7 +73,7 @@ public class DataCaseDetail {
 
     private String repayStatus;//还款状态
 
-    private int newCase;//是否新分配
+    private String newCase;//是否新分配
 
     private int reduceStatus;//减免状态
     private int reportStatus;//报备状态
