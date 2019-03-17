a0891edd4f0b50ee726363aaa730c6ec3b1d5807
齐飞 <qifei@ovfintech.com>
2019-03-17 21:23:03 +0800

    update

--- src/main/java/xyz/zaijushou/zhx/constant/ExcelRepayRecordConstant.java
+++ src/main/java/xyz/zaijushou/zhx/constant/ExcelRepayRecordConstant.java
@@ -190,6 +190,7 @@ public class ExcelRepayRecordConstant {
         COLUMN7("dataCase.client","c.client"),
         COLUMN8("dataCase.overdueBillTime","c.account_age"),
         COLUMN9("dataCase.moneyMsg","c.money"),
+        COLUMN25("dataCase.enRepayAmt","c.en_repay_amt"),
         COLUMN10("repayMoneyMsg","r.repay_money"),
         COLUMN11("dataCase.overdueBalanceMsg",""),
         COLUMN12("repayDate","r.repay_date"),
