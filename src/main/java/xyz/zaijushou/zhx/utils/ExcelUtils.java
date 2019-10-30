5e2ae1c756d1b16bf35aa279f4d9d03dc3af49e0
snow <717688908@qq.com>
2019-10-30 20:38:55 +0800

    update code

--- src/main/java/xyz/zaijushou/zhx/utils/ExcelUtils.java
+++ src/main/java/xyz/zaijushou/zhx/utils/ExcelUtils.java
@@ -67,122 +67,136 @@ public class ExcelUtils {
 
     public static <T> List<T> importExcel(MultipartFile file, ExcelEnum[] enums, Class<T> entityClazz) throws IOException {
         List<T> resultList = new ArrayList<>();
-        InputStream inputStream = file.getInputStream();
-        String fileName = file.getOriginalFilename();
-        Workbook workbook;
-        if(StringUtils.isNotEmpty(fileName) && fileName.length() >= 5 && ".xlsx".equals(fileName.substring(fileName.length() - 5))) {
-            workbook = new XSSFWorkbook(inputStream);
-        } else {
-            workbook = new HSSFWorkbook(inputStream);
-        }
-        evaluator = workbook.getCreationHelper().createFormulaEvaluator();
-        Map<String, ExcelEnum> excelEnumMap = new HashMap<>();
-        for (ExcelEnum value : enums) {
-            excelEnumMap.put(value.getCol(), value);
-        }
-        Sheet sheet = workbook.getSheetAt(0);
-        Row header = sheet.getRow(0);
-        Map<Integer, ExcelEnum> colMap = new HashMap<>();
-        logger.debug("列数：{}", header.getLastCellNum());
-        for (int i = 0; i < header.getLastCellNum(); i++) {
-            Cell cell = header.getCell(i);
-            if(cell == null) {
-                colMap.put(i, null);
-                continue;
+        InputStream inputStream=null;
+        try {
+            inputStream = file.getInputStream();
+            String fileName = file.getOriginalFilename();
+            Workbook workbook;
+            if (StringUtils.isNotEmpty(fileName) && fileName.length() >= 5 && ".xlsx".equals(fileName.substring(fileName.length() - 5))) {
+                workbook = new XSSFWorkbook(inputStream);
+            } else {
+                workbook = new HSSFWorkbook(inputStream);
             }
-            String cellValue = cell.getStringCellValue();
+            evaluator = workbook.getCreationHelper().createFormulaEvaluator();
+            Map<String, ExcelEnum> excelEnumMap = new HashMap<>();
+            for (ExcelEnum value : enums) {
+                excelEnumMap.put(value.getCol(), value);
+            }
+            Sheet sheet = workbook.getSheetAt(0);
+            Row header = sheet.getRow(0);
+            Map<Integer, ExcelEnum> colMap = new HashMap<>();
+            logger.debug("列数：{}", header.getLastCellNum());
+            for (int i = 0; i < header.getLastCellNum(); i++) {
+                Cell cell = header.getCell(i);
+                if (cell == null) {
+                    colMap.put(i, null);
+                    continue;
+                }
+                String cellValue = cell.getStringCellValue();
 
-            colMap.put(i, excelEnumMap.get(cellValue.toString().trim()));
-        }
-        for (int i = 1; i <= sheet.getPhysicalNumberOfRows()-1; i++) {
-            //列，在此处定义是为了准确提示
-            int k =0;
-            try {
-                Row row = sheet.getRow(i);
-                T entity = entityClazz.getConstructor().newInstance();
-                if (row==null){
-                    entity = null;
-                    continue;
-                }
-                if ((row.getCell(0)==null || row.getCell(0).toString().equals("")) && (row.getCell(1)==null ||  row.getCell(1).toString().equals("")) && (row.getCell(2)==null || row.getCell(2).toString().equals(""))  && (row.getCell(3)==null ||  row.getCell(3).toString().equals(""))){
-                    entity = null;
-                    continue;
-                }
-                for (k = 0; k < header.getLastCellNum(); k++) {
-                    Cell cell = row.getCell(k);
+                colMap.put(i, excelEnumMap.get(cellValue.toString().trim()));
+            }
+            for (int i = 1; i <= sheet.getPhysicalNumberOfRows() - 1; i++) {
+                //列，在此处定义是为了准确提示
+                int k = 0;
+                try {
+                    Row row = sheet.getRow(i);
+                    T entity = entityClazz.getConstructor().newInstance();
+                    if (row == null) {
+                        entity = null;
+                        continue;
+                    }
+                    if ((row.getCell(0) == null || row.getCell(0).toString().equals("")) && (row.getCell(1) == null || row.getCell(1).toString().equals("")) && (row.getCell(2) == null || row.getCell(2).toString().equals("")) && (row.getCell(3) == null || row.getCell(3).toString().equals(""))) {
+                        entity = null;
+                        continue;
+                    }
+                    for (k = 0; k < header.getLastCellNum(); k++) {
+                        Cell cell = row.getCell(k);
                     /*if (k==92){
                         System.out.println(111);
                     }*/
-                    ExcelEnum excelEnum = colMap.get(k);
-                    if (excelEnum == null) {
-                        continue;
-                    }
-                    String attr = excelEnum.getAttr();
-                    Matcher matcher = Pattern.compile("\\[\\d\\]\\.").matcher(attr);
-                    if (matcher.find()) {
-                        int index = Integer.parseInt(matcher.group(0).substring(1, 2));
-                        String firstAttr = attr.substring(0, matcher.start());
-                        String secondAttr = attr.substring(matcher.end());
-                        Method firstAttrGetMethod = entityClazz.getMethod("get" + firstAttr.substring(0, 1).toUpperCase() + firstAttr.substring(1));
-                        Object object = firstAttrGetMethod.invoke(entity);
-                        if (object == null) {
-                            Method firstAttrSetMethod = entityClazz.getMethod("set" + firstAttr.substring(0, 1).toUpperCase() + firstAttr.substring(1), List.class);
-                            firstAttrSetMethod.invoke(entity, new ArrayList<>());
+                        ExcelEnum excelEnum = colMap.get(k);
+                        if (excelEnum == null) {
+                            continue;
                         }
-                        List subList = (List) firstAttrGetMethod.invoke(entity);
-                        if (subList.size() == index) {
-                            Class clazz = excelEnum.getAttrClazz()[0];
-                            Constructor constructor = clazz.getConstructor();
-                            Object obj = constructor.newInstance();
-                            subList.add(obj);
-                        }
-                        Method secondAttrSetMethod = excelEnum.getAttrClazz()[0].getMethod("set" + secondAttr.substring(0, 1).toUpperCase() + secondAttr.substring(1), excelEnum.getAttrClazz()[1]);
-
-                        secondAttrSetMethod.invoke(subList.get(index), cellValue(cell, excelEnum.getAttrClazz()[1],i,excelEnum.getCol()));
-                    } else {
-                        matcher = Pattern.compile("\\.").matcher(attr);
+                        String attr = excelEnum.getAttr();
+                        Matcher matcher = Pattern.compile("\\[\\d\\]\\.").matcher(attr);
                         if (matcher.find()) {
+                            int index = Integer.parseInt(matcher.group(0).substring(1, 2));
                             String firstAttr = attr.substring(0, matcher.start());
                             String secondAttr = attr.substring(matcher.end());
                             Method firstAttrGetMethod = entityClazz.getMethod("get" + firstAttr.substring(0, 1).toUpperCase() + firstAttr.substring(1));
                             Object object = firstAttrGetMethod.invoke(entity);
                             if (object == null) {
+                                Method firstAttrSetMethod = entityClazz.getMethod("set" + firstAttr.substring(0, 1).toUpperCase() + firstAttr.substring(1), List.class);
+                                firstAttrSetMethod.invoke(entity, new ArrayList<>());
+                            }
+                            List subList = (List) firstAttrGetMethod.invoke(entity);
+                            if (subList.size() == index) {
                                 Class clazz = excelEnum.getAttrClazz()[0];
                                 Constructor constructor = clazz.getConstructor();
                                 Object obj = constructor.newInstance();
-                                Method firstAttrSetMethod = entityClazz.getMethod("set" + firstAttr.substring(0, 1).toUpperCase() + firstAttr.substring(1), excelEnum.getAttrClazz()[0]);
-                                firstAttrSetMethod.invoke(entity, obj);
+                                subList.add(obj);
                             }
-                            object = firstAttrGetMethod.invoke(entity);
                             Method secondAttrSetMethod = excelEnum.getAttrClazz()[0].getMethod("set" + secondAttr.substring(0, 1).toUpperCase() + secondAttr.substring(1), excelEnum.getAttrClazz()[1]);
-                            secondAttrSetMethod.invoke(object, cellValue(cell, excelEnum.getAttrClazz()[1],i,excelEnum.getCol()));
-                        } else {
-                            Method method = entity.getClass().getMethod("set" + attr.substring(0, 1).toUpperCase() + attr.substring(1), excelEnum.getAttrClazz()[0]);
-                            method.invoke(entity, cellValue(cell, excelEnum.getAttrClazz()[0],i,excelEnum.getCol()));
-                        }
-                    }
 
+                            secondAttrSetMethod.invoke(subList.get(index), cellValue(cell, excelEnum.getAttrClazz()[1], i, excelEnum.getCol()));
+                        } else {
+                            matcher = Pattern.compile("\\.").matcher(attr);
+                            if (matcher.find()) {
+                                String firstAttr = attr.substring(0, matcher.start());
+                                String secondAttr = attr.substring(matcher.end());
+                                Method firstAttrGetMethod = entityClazz.getMethod("get" + firstAttr.substring(0, 1).toUpperCase() + firstAttr.substring(1));
+                                Object object = firstAttrGetMethod.invoke(entity);
+                                if (object == null) {
+                                    Class clazz = excelEnum.getAttrClazz()[0];
+                                    Constructor constructor = clazz.getConstructor();
+                                    Object obj = constructor.newInstance();
+                                    Method firstAttrSetMethod = entityClazz.getMethod("set" + firstAttr.substring(0, 1).toUpperCase() + firstAttr.substring(1), excelEnum.getAttrClazz()[0]);
+                                    firstAttrSetMethod.invoke(entity, obj);
+                                }
+                                object = firstAttrGetMethod.invoke(entity);
+                                Method secondAttrSetMethod = excelEnum.getAttrClazz()[0].getMethod("set" + secondAttr.substring(0, 1).toUpperCase() + secondAttr.substring(1), excelEnum.getAttrClazz()[1]);
+                                secondAttrSetMethod.invoke(object, cellValue(cell, excelEnum.getAttrClazz()[1], i, excelEnum.getCol()));
+                            } else {
+                                Method method = entity.getClass().getMethod("set" + attr.substring(0, 1).toUpperCase() + attr.substring(1), excelEnum.getAttrClazz()[0]);
+                                method.invoke(entity, cellValue(cell, excelEnum.getAttrClazz()[0], i, excelEnum.getCol()));
+                            }
+                        }
+
+                    }
+                    if (entity != null) {
+                        resultList.add(entity);
+                    }
+                } catch (CustomerException al) {
+                    logger.error("检测数据有误：{}", al.getMessage());
+                    throw new CustomerException(500, al.getMsg());
+                } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException | InstantiationException e) {
+                    logger.error("excel解析错误：{}", e);
+                    throw new IOException("excel解析错误", e);
+                } catch (ParseException e) {
+                    logger.error("excel日期解析错误：{}", e);
+                    //+1是为了补充标题所占的一行
+                    int j = i + 1;
+                    throw new IOException("第" + j + "行" + (k + 1) + "列日期解析错误", e);
+                } catch (Exception e) {
+                    logger.error("excel解析错误：{}", e);
+                    throw new IOException("excel解析错误", e);
                 }
-                if (entity!=null) {
-                    resultList.add(entity);
+            }
+            return resultList;
+        }catch (Exception e) {
+            e.printStackTrace();
+        }finally {
+            try {
+                if (inputStream != null) {
+                    inputStream.close();
                 }
-            } catch (CustomerException al){
-                logger.error("检测数据有误：{}", al.getMessage());
-                throw new CustomerException(500,al.getMsg());
-            }catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException | InstantiationException e) {
-                logger.error("excel解析错误：{}", e);
-                throw new IOException("excel解析错误", e);
-            } catch (ParseException e) {
-                logger.error("excel日期解析错误：{}", e);
-                //+1是为了补充标题所占的一行
-                int j = i+1;
-                throw new IOException("第"+j+"行"+(k+1)+"列日期解析错误", e);
-            } catch (Exception e) {
-                logger.error("excel解析错误：{}", e);
-                throw new IOException("excel解析错误", e);
+            } catch (IOException e1) {
+                e1.printStackTrace();
             }
         }
-        return resultList;
+        return null;
     }
 
     @Transactional(rollbackFor = Exception.class)
@@ -191,8 +205,9 @@ public class ExcelUtils {
         WebResponse response = WebResponse.buildResponse();
         int succesLines = 0;
         StringBuffer sucessStr = new StringBuffer("导入成功，总计导入行数为:");
-
-        InputStream inputStream = file.getInputStream();
+        InputStream inputStream=null;
+        try{
+        inputStream = file.getInputStream();
         String fileName = file.getOriginalFilename();
         Workbook workbook;
         if(StringUtils.isNotEmpty(fileName) && fileName.length() >= 5 && ".xlsx".equals(fileName.substring(fileName.length() - 5))) {
@@ -326,6 +341,18 @@ public class ExcelUtils {
 
         response.setMsg(sucessStr.append(succesLines).toString());
         return response;
+        }catch (Exception e) {
+            e.printStackTrace();
+        }finally {
+            try {
+                if (inputStream != null) {
+                    inputStream.close();
+                }
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+        }
+        return null;
     }
 
     private static Object cellValue(Cell cell, Class clazz,int row,String col) throws CustomerException,ParseException {
@@ -667,12 +694,24 @@ public class ExcelUtils {
         response.setHeader("Content-Disposition", "attachment;filename=" + URLEncoder.encode(fileName, "UTF-8"));
         //文件下载类型--二进制文件
         response.setContentType("application/octet-stream");
-        ServletOutputStream servletOutputStream = response.getOutputStream();
+        ServletOutputStream servletOutputStream = null;
+        try{
+        servletOutputStream = response.getOutputStream();
 
         workbook.write(servletOutputStream);
 
         servletOutputStream.flush();
-        servletOutputStream.close();
+        }catch (Exception e) {
+            e.printStackTrace();
+        }finally {
+            try {
+                if (servletOutputStream != null) {
+                    servletOutputStream.close();
+                }
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+        }
 //        File file = new File(fileName);
 //        if (!file.exists()){
 //            file.mkdir();
